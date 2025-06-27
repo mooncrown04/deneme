b8956023bcd3e017d2382dab75521a16b9edddc4
mooncrown04 <mooncrown04@gmail.com>
2025-06-28 01:38:56 +0300

    Update TwitchProvider.kt

--- TwitchProvider/src/main/kotlin/recloudstream/TwitchProvider.kt
+++ TwitchProvider/src/main/kotlin/recloudstream/TwitchProvider.kt
@@ -1,179 +1,216 @@
-package recloudstream
+package com.nikyokki
 
-import com.lagradost.cloudstream3.HomePageList
-import com.lagradost.cloudstream3.HomePageResponse
-import com.lagradost.cloudstream3.LiveSearchResponse
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.fixUrl
-import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newLiveSearchResponse
-import com.lagradost.cloudstream3.newLiveStreamLoadResponse
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.ExtractorLinkType
-import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.utils.newExtractorLink
+import android.util.Log
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.jsoup.nodes.Element
-import java.lang.RuntimeException
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.APIHolder.capitalize
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+import com.lagradost.nicehttp.Requests
 
-class TwitchProvider : MainAPI() {
-    override var mainUrl = "https://twitchtracker.com" // Easiest to scrape
-    override var name = "Twitch"
-    override val supportedTypes = setOf(TvType.Live)
-
-    override var lang = "uni"
-
-    override val hasMainPage = true
-    private val gamesName = "games"
+class SineWix : MainAPI() {
+    override var mainUrl              = "https://ydfvfdizipanel.ru/public/api"
+    override var name                 = "SineWix"
+    override val hasMainPage          = true
+    override var lang                 = "tr"
+    override val hasQuickSearch       = false
+    override val hasChromecastSupport = true
+    override val hasDownloadSupport   = true
+    override val supportedTypes       = setOf(TvType.Movie, TvType.TvSeries, TvType.Anime)
 
     override val mainPage = mainPageOf(
-        "$mainUrl/channels/live" to "Top global live streams",
-        "$mainUrl/games" to gamesName
+        "${mainUrl}/genres/topteen/all"           to "Top 10 Listesi",
+        "${mainUrl}/genres/latestmovies/all"      to "Son Eklenen Filmler",
+        "${mainUrl}/genres/latestseries/all"      to "Son Eklenen Diziler",
+        "${mainUrl}/genres/latestanimes/all"      to "Son Eklenen Animeler",
     )
-    private val isHorizontal = true
+
+    private val key = "9iQNC5HQwPlaFuJDkhncJ5XTJ8feGXOJatAA"
+    private val headers = mapOf(
+        "signature" to "308202c3308201aba0030201020204075cec01300d06092a864886f70d01010b050030123110300e0603550403130753696e65776978301e1" +
+                "70d3231303932313233333334395a170d3436303931353233333334395a30123110300e0603550403130753696e6577697830820122300d06092a864" +
+                "886f70d01010105000382010f003082010a0282010100b0a2a1bc5c3f16f19c3b2456cfd0a6128ced9f5e2e2c4cca1a100e17b07b86256258f372e76" +
+                "a95a17e9e4a1c048e364835723a95e8ef6d5bdfb5694b50277c65a64f7b012fdf164e5dc93629561f6ca29b7dc82ebb3d6f3c8e8fc6795847fe331ad" +
+                "4a13ed6c059a83804c43d3747526d769580f3a4153752eb22dac66dd15f1582caa43305dc49f55ac7b1b89013e654d2ca8c94c30956659674cc67325" +
+                "6c04208f09118bae14cdd72d78f9ee2aece958084a8c2e315deff45726d4fc1f18ec39569ff1abe4f36a8d01090e5f68c07c28763513b88208bcac1a" +
+                "6e1941f6fd8bfdd52f832098ddb2154c8f565bc5d58c7106a19e03787e75c7f34997000e3bcf30203010001a321301f301d0603551d0e04160414b54" +
+                "5fc18e74a791d9402b53940ae38b96e9e209c300d06092a864886f70d01010b05000382010100a8a64d9e7c8b5db102af15d3caf94ff8d3e9be9008b" +
+                "b0021117ca2f0762e68583354b126a041bb1fb6e6308e421e4b5a71f779cde63e5d2fc5976bff966c3c4034e852c077d8e74458fbae2ec1db74b1f40" +
+                "82e188bf8ef7c42a44e3fbfb693bb00ee2a727096b42360ddce1bdcd3536f50c8693bcc62a7b7204bcefe2ecf1f7c820bcd63e1d7a6acc8bf6163086" +
+                "915fc5f607cf51bc7a8635f98bb4c65a8f24b7b5a82c7b06868f565cb0d6ac4775c4aac777536ddd1a565f990fd8cbe539185fa7aab610b7855a687a" +
+                "00f4e55536d72873444552c50fd10727dbf298a9be6ed6ae62148dd1de365f3729915dd31975e28a472d752ac14db3db548405cc31e1e",
+        "hash256" to "f4d4bc98a3fc4600e7f2c2bab7533f1f03d8a70ff03c256bb11dc57050536bd0",
+        "user-agent" to "EasyPlex (Android 13; SM-A546E; samsung; tr)"
+    )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        return when (request.name) {
-            gamesName -> newHomePageResponse(parseGames(), hasNext = false) // Get top games
+        val url = "${request.data}/${key}?page=$page"
+        val document = app.get(url, headers = headers).document
+        val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        val result: SineSonuc = objectMapper.readValue(document.body().text())
+
+        val home = result.data.mapNotNull { it.toMainPageResult() }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun SineData.toMainPageResult(): SearchResponse? {
+        val title     = this.title ?: this.name
+        val posterUrl = this.posterPath
+        val href: String
+        when (this.type) {
+            "movie" -> {
+                href = "${mainUrl}/media/detail/${this.id}/${key}"
+                return newMovieSearchResponse(title!!, href, TvType.Movie) { this.posterUrl = posterUrl }
+            }
+            "serie" -> {
+                href = "${mainUrl}/series/show/${this.id}/${key}"
+                return newTvSeriesSearchResponse(title!!, href, TvType.TvSeries) { this.posterUrl = posterUrl }
+            }
             else -> {
-                val doc = app.get(request.data, params = mapOf("page" to page.toString())).document
-                val channels = doc.select("table#channels tr").map { element ->
-                    element.toLiveSearchResponse()
-                }
-                newHomePageResponse(
-                    listOf(
-                        HomePageList(
-                            request.name,
-                            channels,
-                            isHorizontalImages = isHorizontal
-                        )
-                    ),
-                    hasNext = true
-                )
+                href = "${mainUrl}/animes/show/${this.id}/${key}"
+                return newAnimeSearchResponse(title!!, href, TvType.Anime) { this.posterUrl = posterUrl }
             }
-        }
-    }
-
-    private fun Element.toLiveSearchResponse(): LiveSearchResponse {
-        val anchor = this.select("a")
-        val linkName = anchor.attr("href").substringAfterLast("/")
-        val name = anchor.firstOrNull { it.text().isNotBlank() }?.text()
-        val image = this.select("img").attr("src")
-        return newLiveSearchResponse(
-            name ?: "",
-            linkName,
-            TvType.Live,
-            fix = false
-        ) { posterUrl = image }
-    }
-
-    private suspend fun parseGames(): List<HomePageList> {
-        val doc = app.get("$mainUrl/games").document
-        return doc.select("div.ranked-item")
-            .take(5)
-            .mapNotNull { element -> // No apmap to prevent getting 503 by cloudflare
-                val game = element.select("div.ri-name > a")
-                val url = fixUrl(game.attr("href"))
-                val name = game.text()
-                val searchResponses = parseGame(url).ifEmpty { return@mapNotNull null }
-                HomePageList(name, searchResponses, isHorizontalImages = isHorizontal)
-            }
-    }
-
-    private suspend fun parseGame(url: String): List<LiveSearchResponse> {
-        val doc = app.get(url).document
-        return doc.select("td.cell-slot.sm").map { element ->
-            element.toLiveSearchResponse()
-        }
-    }
-
-    override suspend fun load(url: String): LoadResponse {
-        val realUrl = url.substringAfterLast("/")
-        val doc = app.get("$mainUrl/$realUrl", referer = mainUrl).document
-        val name = doc.select("div#app-title").text()
-        if (name.isBlank()) {
-            throw RuntimeException("Could not load page, please try again.\n")
-        }
-        val rank = doc.select("div.rank-badge > span").last()?.text()?.toIntOrNull()
-        val image = doc.select("div#app-logo > img").attr("src")
-        val poster = doc.select("div.embed-responsive > img").attr("src").ifEmpty { image }
-        val description = doc.select("div[style='word-wrap:break-word;font-size:12px;']").text()
-        val language = doc.select("a.label.label-soft").text().ifEmpty { null }
-        val isLive = doc.select("div.live-indicator-container").isNotEmpty()
-
-        val tags = listOfNotNull(
-            isLive.let { if (it) "Live" else "Offline" },
-            language,
-            rank?.let { "Rank: $it" },
-        )
-
-        val twitchUrl = "https://twitch.tv/$realUrl"
-
-        return newLiveStreamLoadResponse(
-            name, twitchUrl, twitchUrl
-        ) {
-            plot = description
-            posterUrl = image
-            backgroundPosterUrl = poster
-            this@newLiveStreamLoadResponse.tags = tags
         }
     }
 
     override suspend fun search(query: String): List<SearchResponse>? {
-        val document =
-            app.get("$mainUrl/search", params = mapOf("q" to query), referer = mainUrl).document
-        return document.select("table.tops tr").map { it.toLiveSearchResponse() }
+        val document = app.get("${mainUrl}/search/${query}/${key}", headers= headers).document
+        val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        val result: SineSearch = objectMapper.readValue(document.body().text())
+
+        return result.search?.mapNotNull { it.toMainPageResult() }
     }
 
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        return loadExtractor(data, subtitleCallback, callback)
+
+    override suspend fun quickSearch(query: String): List<SearchResponse>? = search(query)
+
+    override suspend fun load(url: String): LoadResponse? {
+        return if (url.contains("/media/detail/")) {
+            movieDetail(url)
+        } else {
+            serieDetail(url)
+        }
     }
 
-    class TwitchExtractor : ExtractorApi() {
-        override val mainUrl = "https://twitch.tv/"
-        override val name = "Twitch"
-        override val requiresReferer = false
+    private suspend fun movieDetail(url: String): LoadResponse? {
+        val document = app.get(url, headers = headers).document
+        val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        val result: SineMovie = objectMapper.readValue(document.body().text())
+        val originalName    = result.originalName
+        val name            = result.title
+        val title           = "$originalName - $name"
+        val poster          = result.backdropPath
+        val description     = result.overview
+        val year            = result.releaseDate?.split("-")?.first()?.toIntOrNull()
+        val tags            = result.genres?.map { it.name }
+        val rating          = result.vote.toRatingInt()
+        val duration        = result.runtime?.toInt()
+        val actors          = result.cast?.map { Actor(it.name!!, it.profilePath) }
+        val trailer         = result.trailer
 
-        data class ApiResponse(
-            val success: Boolean,
-            val urls: Map<String, String>?
-        )
+        return newMovieLoadResponse(title, result.videos?.get(0)?.link!!, TvType.Movie, result.videos[0].link!!) {
+            this.posterUrl       = poster
+            this.plot            = description
+            this.year            = year
+            this.tags            = tags as List<String>?
+            this.rating          = rating
+            this.duration        = duration
+            addActors(actors)
+            addTrailer("https://www.youtube.com/embed/${trailer}")
+        }
+    }
 
-        override suspend fun getUrl(
-            url: String,
-            referer: String?,
-            subtitleCallback: (SubtitleFile) -> Unit,
-            callback: (ExtractorLink) -> Unit
-        ) {
-            val response =
-                app.get("https://pwn.sh/tools/streamapi.py?url=$url").parsed<ApiResponse>()
-            response.urls?.forEach { (name, url) ->
-                val quality = getQualityFromName(name.substringBefore("p"))
-                callback.invoke(
-                    newExtractorLink(
-                        this.name,
-                        "${this.name} ${name.replace("${quality}p", "")}",
-                        url
-                    ) {
-                        this.type = ExtractorLinkType.M3U8
-                        this.quality = quality
-                        this.referer = ""
-                    }
-                )
+    private suspend fun serieDetail(url: String): LoadResponse? {
+        val document = app.get(url, headers = headers).document
+        val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        val result: SineSerie = objectMapper.readValue(document.body().text())
+        val originalName = result.originalName ?: ""
+        val name = result.name ?: ""
+        val title           = "$originalName - $name"
+        Log.d("SWX", "title » $title")
+        val poster          = result.backdropPath
+        Log.d("SWX", "poster » $poster")
+        val description     = result.overview
+        Log.d("SWX", "description » $description")
+        val year            = result.releaseDate?.split("-")?.first()?.toIntOrNull()
+        Log.d("SWX", "year » $year")
+        val tags            = result.genres?.map { it }
+        Log.d("SWX", "tags » $tags")
+        val rating          = result.vote.toRatingInt()
+        Log.d("SWX", "rating » $rating")
+        val actors          = result.cast?.map { Actor(it.name!!, it.profilePath) }
+        Log.d("SWX", "actors » $actors")
+        val trailer         = result.trailer
+        Log.d("SWX", "trailer » $trailer")
+
+        val episodes = mutableListOf<Episode>()
+        result.seasons?.forEach {
+            val seasonNumber = it.seasonNumber
+            Log.d("SWX", "seasonNumber » $seasonNumber")
+            it.episodes?.forEach { ep ->
+                Log.d("SWX", "ep » $ep")
+                if (!ep.videos.isNullOrEmpty()) {
+                    val sineVideo = ep.videos[0]
+                    episodes.add(newEpisode(sineVideo.link) {
+                        this.name = ep.name
+                        this.season = seasonNumber
+                        this.episode = ep.episodeNumber
+                        this.posterUrl = ep.stillPath
+                    })
+                }
             }
         }
+
+        if (url.contains("/series/show/")) {
+            return newTvSeriesLoadResponse(title.trim(), url, TvType.TvSeries, episodes) {
+                this.posterUrl       = poster
+                this.year            = year
+                this.plot            = description
+                this.tags            = tags
+                this.rating          = rating
+                addActors(actors)
+                addTrailer(trailer)
+            }
+        } else {
+            return newTvSeriesLoadResponse(title.trim(), url, TvType.Anime, episodes) {
+                this.posterUrl       = poster
+                this.year            = year
+                this.plot            = description
+                this.tags            = tags
+                this.rating          = rating
+                addActors(actors)
+                addTrailer(trailer)
+            }
+        }
+    }
+
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        Log.d("SWX", "data » $data")
+        if (data.contains("snwaxdop")) {
+            callback.invoke(
+                newExtractorLink(
+                    source = "SineWix",
+                    name = "SineWix",
+                    url = data,
+                    ExtractorLinkType.VIDEO
+                ) {
+                    this. quality = Qualities.Unknown.value
+                    this.headers = headers
+                }
+            )
+        } else {
+            loadExtractor(data, "${mainUrl}/", subtitleCallback, callback)
+        }
+        return true
     }
 }
